5c862e4e58898828a4e3c1c086a6a34de73ca620
hateteen <116436850+hateteen@users.noreply.github.com>
2023-08-03 19:21:56 +0300

    Add files via upload

---
+++ Final_Test/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Укажите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+string[] arr = FirstArray(num);
+Console.WriteLine();
+PrintArray(arr);
+Console.WriteLine();
+int count = CountStringsNewArray(arr);
+if (count > 0)
+{
+    string[] arrNew = FinalArray(arr, count);
+    PrintArray(arrNew);
+}
+else Console.WriteLine("Не нашлось подходящих элементов");
+
+
+
+string[] FirstArray(int number)
+{
+    string[] array = new string[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите строку");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int CountStringsNewArray(string[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) counter++;
+    }
+    return counter;
+}
+
+string[] FinalArray(string[] array, int counter)
+{
+    string[] arrayNew = new string[counter];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            arrayNew[j] = array[i];
+            j++;
+        }
+    }
+    return arrayNew;
+}
